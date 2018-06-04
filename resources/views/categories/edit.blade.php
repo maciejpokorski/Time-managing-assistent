68423bcba6d5d5579a67f181d79bd7abf46dd779
Maciej Pokorski <maciekpokorski@gmail.com>
2018-06-04 20:04:25 +0200

    center content

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app') @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
 
         <div class="col-md-9">
             <div class="card">
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app') @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-9">
+    <div class="row justify-content-center">
+        <div class="col-md-9 ">
             <div class="card">
                 <div class="card-header">Categories</div>
                 <div class="card-body">
