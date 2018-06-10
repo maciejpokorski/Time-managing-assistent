89c76e00bacfb248c31e80942707d33370c1ee7a
Maciej Pokorski <maciekpokorski@gmail.com>
2018-06-10 13:34:26 +0200

    add user relationship to category

--- app/Category.php
+++ app/Category.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['title','color', 'user_id'];
+    protected $fillable = ['title','color'];
 
     public function events()
     {
         return $this->hasMany(Event::class);
+    }
+
+    public function user(){
+        return $this->belongsTo('App\User');
     }
 }
