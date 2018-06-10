6b0904136d7d4a71ff61a253d8b16ffc2fdad4e9
Maciej Pokorski <maciekpokorski@gmail.com>
2018-06-10 14:07:40 +0200

    user ownership middleware

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -14,6 +14,7 @@ class CategoriesController extends Controller
 {
     public function __construct(){
         $this->middleware('auth');
+        $this->middleware('owner:category', ['only' => ['update']]);
     }
 
     /**
